ab5c0736a988b393d892f064ef17e1649168179c
Pawel Piecuch <piecuch.pawel@gmail.com>
2024-12-08 18:34:04 +0100

    cleanup

--- test.sh
+++ test.sh
@@ -4,4 +4,11 @@
 for f in $(seq 0 4); do
     ./devterm_tool "{font=$f}Test font index $f{nl}"
 done
+for f in $(seq 0 4); do
+    ./devterm_tool "{font=${f}a}Test font index $f{nl}"
+done
+for f in $(seq 0 4); do
+    ./devterm_tool "{font=${f}u}Test font index $f{nl}"
+done
+./devterm_tool "{div=$((RANDOM % 10))}"
 ./devterm_tool "{page}"
