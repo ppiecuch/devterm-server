9e391b79d4bc87e7fbf6c70deae8a6a3e7596a58
Pawel Piecuch <piecuch.pawel@gmail.com>
2024-12-09 17:03:21 +0100

    cleanup

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 ./devterm_tool "{div=$((RANDOM % 10)),time}"
+./devterm_tool "{nl}[[ TEST 1 ]]{nl}{nl}"
 for f in $(seq 0 4); do
     ./devterm_tool "{font=$f}Test font index $f{nl}"
 done
+./devterm_tool "{nl}[[ TEST 2 ]]{nl}{nl}"
 for f in $(seq 0 4); do
     ./devterm_tool "{font=${f}a}Test font index $f{nl}"
 done
+./devterm_tool "{nl}[[ TEST 3 ]]{nl}{nl}"
 for f in $(seq 0 4); do
     ./devterm_tool "{font=${f}u}Test font index $f{nl}"
 done
